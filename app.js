7525e52fe5fc6942f59195074734c309c7779919
Jessica Alder <jessalder1989@gmail.com>
2024-04-25 21:00:08 -0700

    Cleaned up readline references preparing to implement inquirer.js

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 import express from "express";
 import "dotenv/config";
-import { input } from '@inquirer/prompts';
 import okta from "./api/middleware/okta_api.js";
 
 const app = express();
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
 
 // okta.createUser('Velvet', 'Velore', 'jessalder1989@gmail.com', `vv@${process.env.OKTA_DOMAIN}`, 'BloodyNights123');
 // okta.createGroup('Vampires', 'A group for blood drinkers.');
